0c5eb7551e3057c4deed3aed2175e1c90ec6696d
SUUDHI <sudhanshumeena123@gmail.com>
2025-02-20 11:29:33 +0530

    add in 02 list code

---
+++ tuple code/05 Swap two tuples.py
@@ -0,0 +1,21 @@
+tuple1 = (11, 22)
+tuple2 = (99, 88)
+
+#my aproch
+temp=tuple1
+tuple1=tuple2
+tuple2=temp
+print(tuple1)
+print(tuple2)
+
+t1 = (110, 220)
+t2 = (990, 880)
+
+t1,t2 = t2,t1
+print(t1)
+print(t2)
+
+
+
+
+
